f06b5ed24b8ff494c852617a2054a235a8627970
Giovane <giovanedaniell@gmail.com>
2023-01-17 10:35:16 -0300

    refactor: change HttpPostClient to receive a params object

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -7,6 +7,8 @@ export default class RemoteAuthentication {
   ) {}
 
   async auth (): Promise<void> {
-    await this.httpPostClient.post(this.url)
+    await this.httpPostClient.post({
+      url: this.url
+    })
   }
 }
