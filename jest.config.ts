f9e2e94587ecc4be4541e88e88abb7af3512007e
Giovane <giovanedaniell@gmail.com>
2023-01-17 10:16:03 -0300

    chore: add moduleDirectories on jest to get src as absolute path

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,15 @@
-import type { Config } from "jest";
+import type { Config } from 'jest'
 
 const config: Config = {
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
   verbose: true,
-  collectCoverageFrom: ["<rootDir>/src/**.*.{ts,tsx}"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  collectCoverageFrom: ['<rootDir>/src/**.*.{ts,tsx}'],
+  moduleDirectories: ['node_modules', '<rootDir>/src', '<rootDir>/.jest'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
-    ".+\\.ts$": "ts-jest",
-  },
-};
+    '.+\\.ts$': 'ts-jest'
+  }
+}
 
-export default config;
+export default config
