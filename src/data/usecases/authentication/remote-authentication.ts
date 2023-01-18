b0e981b85dc7c82928432163135e238043d8eb30
Giovane <giovanedaniell@gmail.com>
2023-01-18 10:35:27 -0300

    feat: ensure RemoteAuthentication returns an AccountModel on success, and throws an error if httoResponse has no body

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,23 +3,30 @@ import { HttpStatusCode } from 'data/protocols/http/http-response'
 import { InvalidCredentialsError } from 'domain/errors/invalid-credentials'
 import { UnexpectedError } from 'domain/errors/unexpected'
 import { AccountModel } from 'domain/models/account'
-import { AuthenticationParams } from 'domain/usecases/authentication'
+import { AuthenticationParams, Authentication } from 'domain/usecases/authentication'
 
-export default class RemoteAuthentication {
+export default class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok:
+        if (!httpResponse.body) {
+          throw new UnexpectedError()
+        }
+
+        return httpResponse.body
+
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+
       default: throw new UnexpectedError()
     }
   }
