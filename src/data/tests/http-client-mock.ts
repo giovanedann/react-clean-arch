227b3c42ff334ad139f77ce6224ab11a164fc780
Giovane <giovanedaniell@gmail.com>
2023-01-18 09:38:40 -0300

    feat(interfaces): create interface for HttpResponse and enum for HttpStatusCodes

--- src/data/tests/http-client-mock.ts
+++ src/data/tests/http-client-mock.ts
@@ -2,13 +2,19 @@ import {
   HttpPostClient,
   HttpPostParams
 } from 'data/protocols/http/http-post-client'
+import { HttpResponse, HttpStatusCode } from 'data/protocols/http/http-response'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   body?: object
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.unauthorized
+  }
 
-  async post (params: HttpPostParams): Promise<void> {
+  async post (params: HttpPostParams): Promise<HttpResponse> {
     this.body = params.body
     this.url = params.url
+
+    return await Promise.resolve(this.response)
   }
 }
