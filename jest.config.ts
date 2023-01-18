8e3d6873e09ae3bb39676417c93dd567e1d82c31
Giovane <giovanedaniell@gmail.com>
2023-01-18 11:01:04 -0300

    fix: adjust jest config not displaying coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,12 @@ import type { Config } from 'jest'
 const config: Config = {
   roots: ['<rootDir>/src'],
   verbose: true,
-  collectCoverageFrom: ['<rootDir>/src/**.*.{ts,tsx}'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/node_modules/',
+    '!<rootDir>/src/**/index.ts'
+  ],
   moduleDirectories: ['node_modules', '<rootDir>/src', '<rootDir>/.jest'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
