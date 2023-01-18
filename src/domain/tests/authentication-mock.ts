81b55c623ada57a1c95841f6fcde9559d63c4c64
Giovane <giovanedaniell@gmail.com>
2023-01-18 10:31:22 -0300

    feat(mocks): create method to mock account model

--- src/domain/tests/authentication-mock.ts
+++ src/domain/tests/authentication-mock.ts
@@ -1,7 +1,12 @@
 import { AuthenticationParams } from 'domain/usecases/authentication'
 import { faker } from '@faker-js/faker'
+import { AccountModel } from 'domain/models/account'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.datatype.uuid()
 })
