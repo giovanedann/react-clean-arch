a6d21582c831eb5a37442a53d713edec5e4a1807
Giovane <giovanedaniell@gmail.com>
2023-01-18 11:56:23 -0300

    chore: move data layer http mocks to its own folder

---
+++ src/data/tests/mocks/http-post-mock.ts
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker'
+import { HttpPostParams } from 'data/protocols/http'
+
+export function mockPostRequest (): HttpPostParams<any> {
+  const randomObject = JSON.parse(faker.datatype.json())
+
+  return {
+    url: faker.internet.url(),
+    body: randomObject
+  }
+}
---
+++ src/data/tests/mocks/index.ts
@@ -0,0 +1,2 @@
+export * from './http-client-mock'
+export * from './http-post-mock'
