df50768b2eaf34576dff6df9a1e2c8be10f2800b
Giovane <giovanedaniell@gmail.com>
2023-01-18 11:42:59 -0300

    tests: add test case to ensure HttpClient post method returns the correct value

--- src/infra/http/client/http-client.spec.ts
+++ src/infra/http/client/http-client.spec.ts
@@ -6,6 +6,12 @@ import { HttpPostParams } from 'data/protocols/http'
 jest.mock('axios')
 
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxiosResolvedValue = {
+  data: JSON.parse(faker.datatype.json()),
+  status: faker.datatype.number()
+}
+
+mockedAxios.post.mockResolvedValue(mockedAxiosResolvedValue)
 
 function createSut (): HttpClient {
   return new HttpClient()
@@ -27,5 +33,16 @@ describe('HttpClient', () => {
     await sut.post(request)
 
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  it('should return the correct statusCode and body', async () => {
+    const sut = createSut()
+    const request = mockPostRequest()
+    const response = await sut.post(request)
+
+    expect(response).toStrictEqual({
+      statusCode: mockedAxiosResolvedValue.status,
+      body: mockedAxiosResolvedValue.data
+    })
   })
 })
