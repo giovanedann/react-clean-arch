40662a4fbf0bf8bade2bdd1fc22ef6fed459f0bd
Giovane <giovanedaniell@gmail.com>
2023-02-22 15:09:29 -0300

    feat: add main file to render the page

---
+++ src/main/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from 'presentation/pages/Login'
+
+const container = document.getElementById('root') as HTMLElement
+
+const root = createRoot(container)
+
+root.render(<Login />)
