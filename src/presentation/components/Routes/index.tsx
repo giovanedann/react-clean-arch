932a2ba3651f396e691a5a63a120839b39bd1479
Giovane <giovanedaniell@gmail.com>
2023-02-23 09:30:33 -0300

    feat: create form context

--- src/presentation/components/Routes/index.tsx
+++ src/presentation/components/Routes/index.tsx
@@ -1,3 +1,4 @@
+import { FormProvider } from 'presentation/contexts/form'
 import Login from 'presentation/pages/Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'styles/global.scss'
@@ -5,9 +6,11 @@ import 'styles/global.scss'
 export default function Router(): JSX.Element {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <FormProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </FormProvider>
     </BrowserRouter>
   )
 }
---
+++ src/presentation/contexts/form.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createContext, ReactNode, useContext, useMemo, useState } from 'react'
+
+type FormContextProps = {
+  isLoading: boolean
+  errorMessage: string
+}
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+const FormContext = createContext({} as FormContextProps)
+
+type FormProviderProps = {
+  children: ReactNode
+}
+
+function FormProvider({ children }: FormProviderProps): JSX.Element {
+  const [isLoading, _setIsLoading] = useState<boolean>(false)
+  const [errorMessage, _setErrorMessage] = useState<string>('')
+
+  const formContextValue = useMemo(
+    () => ({
+      isLoading,
+      errorMessage
+    }),
+    [isLoading, errorMessage]
+  )
+
+  return (
+    <FormContext.Provider value={formContextValue}>
+      {children}
+    </FormContext.Provider>
+  )
+}
+
+const useForm = (): FormContextProps => useContext(FormContext)
+
+export { FormProvider, useForm }
