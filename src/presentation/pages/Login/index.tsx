4bf3ffc66c9dce22e5d1aef15bfd1973dff0fc71
Giovane <giovanedaniell@gmail.com>
2023-02-23 10:54:19 -0300

    feat(components): add validation prop on Login

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -4,17 +4,50 @@ import Footer from 'presentation/components/Footer'
 import Input from 'presentation/components/Input'
 import Loader from 'presentation/components/Loader'
 import { useForm } from 'presentation/contexts/form'
+import { Validation } from 'presentation/protocols/validation'
+import { ChangeEvent, useEffect, useState } from 'react'
 
-function Login(): JSX.Element {
+type Props = {
+  validation: Validation
+}
+
+function Login({ validation }: Props): JSX.Element {
   const { errorMessage, isLoading } = useForm()
+
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  useEffect(() => {
+    validation.validate('email', email)
+  }, [email])
+
+  useEffect(() => {
+    validation.validate('password', password)
+  }, [password])
 
   return (
     <div className={styles.login}>
       <Header />
       <form className={styles.form}>
         <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
+        <Input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setEmail(event.target.value)
+          }}
+          placeholder="Digite seu e-mail"
+        />
+        <Input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setPassword(event.target.value)
+          }}
+          placeholder="Digite sua senha"
+        />
         <button className={styles.submit} type="submit" disabled>
           Entrar
         </button>
