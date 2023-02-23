583976a086de013cba4424ddc65dea65c9da105c
Giovane <giovanedaniell@gmail.com>
2023-02-23 09:34:19 -0300

    feat: add error message on Form

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -3,8 +3,11 @@ import Header from 'presentation/components/Header'
 import Footer from 'presentation/components/Footer'
 import Input from 'presentation/components/Input'
 import Loader from 'presentation/components/Loader'
+import { useForm } from 'presentation/contexts/form'
 
 function Login(): JSX.Element {
+  const { errorMessage, isLoading } = useForm()
+
   return (
     <div className={styles.login}>
       <Header />
@@ -12,11 +15,12 @@ function Login(): JSX.Element {
         <h2>Login</h2>
         <Input type="email" name="email" placeholder="Digite seu e-mail" />
         <Input type="password" name="password" placeholder="Digite sua senha" />
-        <button className={styles.submit} type="submit">
+        <button className={styles.submit} type="submit" disabled>
           Entrar
         </button>
         <span className={styles.link}>Criar conta</span>
-        <Loader message="Carregando..." />
+        {isLoading && <Loader message="Carregando..." />}
+        {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
       </form>
       <Footer />
     </div>
