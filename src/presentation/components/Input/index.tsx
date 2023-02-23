1e6aaafaa7f2291454b6ff42c75e8a8125d18b19
Giovane <giovanedaniell@gmail.com>
2023-02-23 10:12:07 -0300

    feat: add tooltip on Input

--- src/presentation/components/Input/index.tsx
+++ src/presentation/components/Input/index.tsx
@@ -1,13 +1,27 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import { ErrorMessages } from 'presentation/contexts/form'
 import { InputHTMLAttributes } from 'react'
 import styles from './styles.scss'
 
 type Props = InputHTMLAttributes<HTMLInputElement> & {}
 
 export default function Input({ ...props }: Props): JSX.Element {
+  const error = ErrorMessages[props.name as keyof typeof ErrorMessages]
+
+  function getStatus(): string {
+    return '🔴'
+  }
+
+  function getTitle(): string {
+    return error
+  }
+
   return (
     <div className={styles.inputWrapper}>
       <input {...props} autoComplete="off" />
-      <span className={styles.status}>🔴</span>
+      <span title={getTitle()} className={styles.status}>
+        {getStatus()}
+      </span>
     </div>
   )
 }
