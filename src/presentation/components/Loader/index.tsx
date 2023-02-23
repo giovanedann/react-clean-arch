dc14e593f43e64f8d4444f6434c1d3021c9c9053
Giovane <giovanedaniell@gmail.com>
2023-02-23 09:30:51 -0300

    refactor: change strategy of Loader

--- src/presentation/components/Loader/index.tsx
+++ src/presentation/components/Loader/index.tsx
@@ -2,14 +2,14 @@ import Spinner from 'presentation/components/Spinner'
 import styles from './styles.scss'
 
 type LoaderProps = {
-  message?: string
+  message: string
 }
 
 export default function Loader({ message }: LoaderProps): JSX.Element {
   return (
     <div className={styles.loaderWrapper} role="loader">
       <Spinner className={styles.spinner} />
-      {message && <span className={styles.message}>{message}</span>}
+      <span className={styles.message}>{message}</span>
     </div>
   )
 }
