c3100814d9e4fc1b1a8614e108f4bae0502c4ecc
Giovane <giovanedaniell@gmail.com>
2023-02-25 18:11:00 -0300

    feat: ensure RequiredFieldValidation validate method returns null if value is not empty

--- src/validation/required-field/required-field.ts
+++ src/validation/required-field/required-field.ts
@@ -4,7 +4,7 @@ import { FieldValidation } from 'validation/protocols/field-validation'
 export class RequiredFieldValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
-  validate(value: string): Error {
-    return new RequiredFieldError()
+  validate(value: string): Error | null {
+    return value ? null : new RequiredFieldError()
   }
 }
