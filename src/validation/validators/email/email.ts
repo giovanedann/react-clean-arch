40c5a6852c2472415b352b1aa4eac00271219437
Giovane <giovanedaniell@gmail.com>
2023-02-25 18:41:49 -0300

    feat: ensure EmailValidation returns null if email is valid

--- src/validation/validators/email/email.ts
+++ src/validation/validators/email/email.ts
@@ -5,6 +5,8 @@ export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
   validate(email: string): InvalidFieldError | null {
-    return new InvalidFieldError('email')
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    return emailRegex.test(email) ? null : new InvalidFieldError(this.field)
   }
 }
