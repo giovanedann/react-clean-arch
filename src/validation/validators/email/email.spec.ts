35e5f5befc0d99fdcea1f55983759f903dd360c8
Giovane <giovanedaniell@gmail.com>
2023-02-25 18:43:44 -0300

    refactor: move EmailValidation creation to a factory method

--- src/validation/validators/email/email.spec.ts
+++ src/validation/validators/email/email.spec.ts
@@ -2,15 +2,19 @@ import { faker } from '@faker-js/faker'
 import { InvalidFieldError } from 'validation/errors'
 import { EmailValidation } from 'validation/validators/email/email'
 
+function createSut(): EmailValidation {
+  return new EmailValidation(faker.database.column())
+}
+
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidation(faker.random.word())
+    const sut = createSut()
     const error = sut.validate(faker.random.word())
     expect(error).toBeInstanceOf(InvalidFieldError)
   })
 
   it('should return null if email is valid', () => {
-    const sut = new EmailValidation(faker.random.word())
+    const sut = createSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeNull()
   })
