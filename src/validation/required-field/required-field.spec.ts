ec5870d19b7f63a2072ec3d9dc6284579b6fa361
Giovane <giovanedaniell@gmail.com>
2023-02-25 18:15:44 -0300

    refactor: add factory method to create RequiredFieldValidation sut

--- src/validation/required-field/required-field.spec.ts
+++ src/validation/required-field/required-field.spec.ts
@@ -2,15 +2,19 @@ import { faker } from '@faker-js/faker'
 import { RequiredFieldError } from 'validation/errors/required-field'
 import { RequiredFieldValidation } from 'validation/required-field/required-field'
 
+function createSut(): RequiredFieldValidation {
+  return new RequiredFieldValidation(faker.database.column())
+}
+
 describe('RequiredFieldValidation', () => {
   it('should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = createSut()
     const error = sut.validate('')
     expect(error).toBeInstanceOf(RequiredFieldError)
   })
 
   it('should return null if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = createSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeNull()
   })
