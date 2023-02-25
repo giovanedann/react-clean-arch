d683c7645e44e2a38e62ba1c50460939c855a16e
Giovane <giovanedaniell@gmail.com>
2023-02-25 18:41:54 -0300

    test: ensure EmailValidation returns null if email is valid

--- src/validation/validators/email/email.spec.ts
+++ src/validation/validators/email/email.spec.ts
@@ -1,10 +1,17 @@
+import { faker } from '@faker-js/faker'
 import { InvalidFieldError } from 'validation/errors'
 import { EmailValidation } from 'validation/validators/email/email'
 
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('invalidemail')
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.random.word())
     expect(error).toBeInstanceOf(InvalidFieldError)
+  })
+
+  it('should return null if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeNull()
   })
 })
