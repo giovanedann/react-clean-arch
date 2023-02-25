2a86510521774d496296f3fea4e316a5277c7a95
Giovane <giovanedaniell@gmail.com>
2023-02-25 19:29:56 -0300

    refactor: change MinLengthValidation factory to be more dynamic

--- src/validation/validators/min-length/min-length.spec.ts
+++ src/validation/validators/min-length/min-length.spec.ts
@@ -1,21 +1,31 @@
+import { faker } from '@faker-js/faker'
 import { InvalidLengthError } from 'validation/errors'
 import { MinLengthValidation } from './min-length'
 
-function createSut(): MinLengthValidation {
-  return new MinLengthValidation('password', 6)
+type SutTypes = {
+  sut: MinLengthValidation
+  minValidLength: number
+  field: string
+}
+
+function createSut(minValidLength: number): SutTypes {
+  const field = faker.database.column()
+  const sut = new MinLengthValidation(field, minValidLength)
+
+  return { sut, field, minValidLength }
 }
 
 describe('MinLengthValidation', () => {
   it('should return error if field length is less than constructor length', () => {
-    const sut = createSut()
-    const error = sut.validate('12345') as Error
+    const { sut, field, minValidLength } = createSut(6)
+    const error = sut.validate(faker.random.alphaNumeric(5)) as Error
     expect(error).toBeInstanceOf(InvalidLengthError)
-    expect(error.message).toEqual('Value provided to password should have at least 6 characters')
+    expect(error.message).toEqual(`Value provided to ${field} should have at least ${minValidLength} characters`)
   })
 
   it('should return null if the field length is higher or equal the constructor length', () => {
-    const sut = createSut()
-    const error = sut.validate('1234567')
+    const { sut } = createSut(6)
+    const error = sut.validate(faker.random.alphaNumeric(7))
     expect(error).toBeNull()
   })
 })
