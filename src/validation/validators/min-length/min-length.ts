7ff2eb924c9a560575e1893a61a8983384112779
Giovane <giovanedaniell@gmail.com>
2023-02-25 19:20:26 -0300

    feat: improve code readability on validate method of MinLengthValidation

--- src/validation/validators/min-length/min-length.ts
+++ src/validation/validators/min-length/min-length.ts
@@ -5,6 +5,7 @@ export class MinLengthValidation implements FieldValidation {
   constructor(readonly field: string, private readonly minValidLength: number) {}
 
   validate(value: string): Error | null {
-    return value.length >= this.minValidLength ? null : new InvalidLengthError(this.field, this.minValidLength)
+    const isLengthValid = value.length >= this.minValidLength
+    return isLengthValid ? null : new InvalidLengthError(this.field, this.minValidLength)
   }
 }
