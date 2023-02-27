7a93802879f2a461aaef5649e3b606673bd3ba85
Giovane <giovanedaniell@gmail.com>
2023-02-27 09:59:55 -0300

    refactor: add exception handle case to HttpClient

--- src/infra/http/client/http-client.ts
+++ src/infra/http/client/http-client.ts
@@ -1,13 +1,23 @@
-import { HttpPostClient, HttpPostParams, HttpResponse } from 'data/protocols/http'
-import axios from 'axios'
+import {
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse
+} from 'data/protocols/http'
+import axios, { AxiosResponse } from 'axios'
 
 export class HttpClient implements HttpPostClient<any, any> {
-  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body)
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    let httpResponse: AxiosResponse<any>
 
-    return ({
+    try {
+      httpResponse = await axios.post(params.url, params.body)
+    } catch (error: any) {
+      httpResponse = error.response
+    }
+
+    return {
       statusCode: httpResponse.status,
       body: httpResponse.data
-    })
+    }
   }
 }
