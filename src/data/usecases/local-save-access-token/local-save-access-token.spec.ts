189f7111e5ffbcb1bcfa2cfa1a85da95983c4162
Giovane <giovanedaniell@gmail.com>
2023-02-27 15:11:24 -0300

    refactor: add factory method to create LocalSaveAccessToken sut

--- src/data/usecases/local-save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/local-save-access-token/local-save-access-token.spec.ts
@@ -2,11 +2,23 @@ import { faker } from '@faker-js/faker'
 import { SetStorageSpy } from 'tests/mocks/data/protocols/cache/set-storage'
 import { LocalSaveAccessToken } from './local-save-access-token'
 
+type SutTypes = {
+  setStorage: SetStorageSpy
+  sut: LocalSaveAccessToken
+  accessToken: string
+}
+
+function createSut(): SutTypes {
+  const setStorage = new SetStorageSpy()
+  const sut = new LocalSaveAccessToken(setStorage)
+  const accessToken = faker.datatype.uuid()
+
+  return { setStorage, sut, accessToken }
+}
+
 describe('LocalSaveAccessToken', () => {
   it('should call SetStorage with correct values', async () => {
-    const setStorage = new SetStorageSpy()
-    const sut = new LocalSaveAccessToken(setStorage)
-    const accessToken = faker.datatype.uuid()
+    const { sut, accessToken, setStorage } = createSut()
     await sut.save(accessToken)
     expect(setStorage.key).toBe('accessToken')
     expect(setStorage.value).toBe(accessToken)
