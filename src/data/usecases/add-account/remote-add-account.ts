b6f210c6e6e9b5898e7470e9552aceea263b7e66
Giovane <giovanedaniell@gmail.com>
2023-02-28 15:23:13 -0300

    feat: ensure RemoteAddAccount throws UnexpectedError on 400

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,5 +1,5 @@
 import { HttpStatusCode, type HttpPostClient } from 'data/protocols/http'
-import { EmailAlreadyInUseError } from 'domain/errors'
+import { EmailAlreadyInUseError, UnexpectedError } from 'domain/errors'
 import { type AccountModel } from 'domain/models'
 import { type AddAccountParams, type AddAccount } from 'domain/usecases'
 
@@ -10,14 +10,20 @@ export class RemoteAddAccount implements AddAccount {
   ) {}
 
   async add(params: AddAccountParams): Promise<AccountModel> {
-    const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return { accessToken: '' }
+
       case HttpStatusCode.forbidden:
         throw new EmailAlreadyInUseError()
 
       default:
-        return { accessToken: 'aaa' }
+        throw new UnexpectedError()
     }
   }
 }
