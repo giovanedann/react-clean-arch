37f21e33b5c61b6af7d46440014754210a9d7d6c
Giovane <giovanedaniell@gmail.com>
2023-03-01 20:14:02 -0300

    feat: ensure errors are added if validation of the SignUp inputs fail

--- src/presentation/pages/SignUp/index.tsx
+++ src/presentation/pages/SignUp/index.tsx
@@ -14,16 +14,69 @@ import {
   INITIAL_DATA_STATE,
   INITIAL_ERRORS_STATE
 } from './reducers'
+import { type Validation } from 'presentation/protocols/validation'
+import { type SaveAccessToken, type Authentication } from 'domain/usecases'
 
-function SignUp(): JSX.Element {
+type Props = {
+  validation: Validation
+  authentication?: Authentication
+  saveAccessToken?: SaveAccessToken
+}
+
+function SignUp({ validation }: Props): JSX.Element {
   const [data, dispatchData] = useReducer(dataReducer, INITIAL_DATA_STATE)
   const [errors, dispatchError] = useReducer(errorReducer, INITIAL_ERRORS_STATE)
 
   const { isLoading, errorMessage } = useForm()
 
+  // Use effect to add and remove errors based on input value
   useEffect(() => {
-    dispatchError({ type: 'EMAIL', payload: 'Email is required' })
-  }, [])
+    const emailError = validation.validate('email', data.email)
+    const nameError = validation.validate('name', data.name)
+    const passwordError = validation.validate('password', data.password)
+    const passwordConfirmationError = validation.validate(
+      'passwordConfirmation',
+      data.passwordConfirmation
+    )
+
+    if (!emailError && errors.emailError) {
+      dispatchError({ type: 'EMAIL', payload: '' })
+    }
+
+    if (!nameError && errors.nameError) {
+      dispatchError({ type: 'NAME', payload: '' })
+    }
+
+    if (!passwordError && errors.passwordError) {
+      dispatchError({ type: 'PASSWORD', payload: '' })
+    }
+
+    if (!passwordConfirmationError && errors.passwordConfirmationError) {
+      dispatchError({
+        type: 'PASSWORD_CONFIRMATION',
+        payload: ''
+      })
+    }
+
+    if (emailError && !errors.emailError) {
+      dispatchError({ type: 'EMAIL', payload: 'Email is required' })
+    }
+
+    if (nameError && !errors.nameError) {
+      dispatchError({ type: 'NAME', payload: 'Name is required' })
+    }
+
+    if (passwordError && !errors.passwordError) {
+      dispatchError({ type: 'PASSWORD', payload: 'Password is required' })
+    }
+
+    if (passwordConfirmationError && !errors.passwordConfirmationError) {
+      dispatchError({
+        type: 'PASSWORD_CONFIRMATION',
+        payload: 'Passwords not matching'
+      })
+    }
+  }, [errors, dispatchError, validation])
 
   return (
     <div className={styles.login}>
@@ -38,7 +91,7 @@ function SignUp(): JSX.Element {
             dispatchData({ type: 'NAME', payload: event.target.value })
           }}
           placeholder="Digite seu nome"
-          errorMessage={errors.emailError}
+          errorMessage={errors.nameError}
         />
         <Input
           type="email"
@@ -71,7 +124,7 @@ function SignUp(): JSX.Element {
             })
           }}
           placeholder="Repita sua senha"
-          errorMessage={errors.passwordError}
+          errorMessage={errors.passwordConfirmationError}
         />
         <button
           className={styles.submit}
