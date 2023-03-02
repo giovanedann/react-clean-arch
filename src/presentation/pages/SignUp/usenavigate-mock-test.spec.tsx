53c7b8a4cbb0b9243e7a7b4f53683f33a546b00c
Giovane <giovanedaniell@gmail.com>
2023-03-02 11:49:00 -0300

    refactor: move test to a sepparated folder to mock useNavigate

---
+++ src/presentation/pages/SignUp/usenavigate-mock-test.spec.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom'
+import 'jest-localstorage-mock'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import createSignUpSut from 'tests/mocks/presentation/SignUp/createSignUpSut'
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn()
+}))
+
+describe('<Login />', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+    jest.restoreAllMocks()
+  })
+
+  it('should show loader during submit request', async () => {
+    const user = userEvent.setup()
+    createSignUpSut({})
+
+    await user.click(screen.getByRole('button', { name: /sign up/i }))
+
+    expect(screen.getByText(/loading\.../i)).toBeInTheDocument()
+  })
+})
