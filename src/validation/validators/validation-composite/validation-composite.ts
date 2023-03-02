267ec8e077b99c463b12d4a81d41b5a2bd6c5479
Giovane <giovanedaniell@gmail.com>
2023-03-02 15:08:29 -0300

    refactor: add generics to ValidationComposite and change strategy

--- src/validation/validators/validation-composite/validation-composite.ts
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -1,15 +1,19 @@
 import { type Validation } from 'presentation/protocols/validation'
 import { type FieldValidation } from 'validation/protocols/field-validation'
 
-export class ValidationComposite implements Validation {
-  private constructor (private readonly validators: FieldValidation[]) {}
+export class ValidationComposite<T> implements Validation<T> {
+  private constructor(private readonly validators: Array<FieldValidation<T>>) {}
 
-  static build (validators: FieldValidation[]): ValidationComposite {
+  static build(
+    validators: Array<FieldValidation<any>>
+  ): ValidationComposite<any> {
     return new ValidationComposite(validators)
   }
 
-  validate(fieldName: string, fieldValue: string): string {
-    const validators = this.validators.filter((validator) => validator.field === fieldName)
+  validate(fieldName: string, fieldValue: T): string {
+    const validators = this.validators.filter(
+      (validator) => validator.field === fieldName
+    )
 
     for (const validator of validators) {
       const error = validator.validate(fieldValue)
