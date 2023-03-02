995ba8f8ba4502afb1560fe875864962896d0370
Giovane <giovanedaniell@gmail.com>
2023-03-02 14:59:23 -0300

    test: add generic to CompareFieldValidation and change strategy

--- src/validation/validators/compare-fields/compare-fields.spec.ts
+++ src/validation/validators/compare-fields/compare-fields.spec.ts
@@ -2,22 +2,36 @@ import { faker } from '@faker-js/faker'
 import { InvalidFieldError } from 'validation/errors'
 import { CompareFieldsValidation } from './compare-fields'
 
-function createSut(compareWith: string): CompareFieldsValidation {
-  return new CompareFieldsValidation(faker.random.word(), compareWith)
+function createSut(
+  field: string,
+  compareWith: string
+): CompareFieldsValidation<any> {
+  return new CompareFieldsValidation(field, compareWith)
 }
 
-describe('RequiredFieldValidation', () => {
+describe('CompareFieldsValidation', () => {
   it('should return error if fields are not equal', () => {
-    const sut = createSut(faker.random.word())
-    const error = sut.validate(faker.random.word())
+    const field1 = faker.random.word()
+    const field2 = faker.database.column()
+    const sut = createSut(field1, field2)
+    const error = sut.validate({
+      [field1]: faker.random.alphaNumeric(5),
+      [field2]: faker.random.alphaNumeric(7)
+    })
 
     expect(error).toBeInstanceOf(InvalidFieldError)
   })
 
   it('should return null if fields are equal', () => {
-    const value = faker.random.word()
-    const sut = createSut(value)
-    const validation = sut.validate(value)
+    const field1 = faker.random.word()
+    const field2 = faker.database.column()
+    const equalValue = faker.random.alphaNumeric(5)
+    const sut = createSut(field1, field2)
+
+    const validation = sut.validate({
+      [field1]: equalValue,
+      [field2]: equalValue
+    })
 
     expect(validation).toBeNull()
   })
