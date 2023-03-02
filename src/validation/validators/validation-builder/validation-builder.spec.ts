33a3869e36c58ff153a6d45ef464fa5754d73c14
Giovane <giovanedaniell@gmail.com>
2023-03-02 15:07:48 -0300

    test: add generics to ValidatioBuilder and change strategy

--- src/validation/validators/validation-builder/validation-builder.spec.ts
+++ src/validation/validators/validation-builder/validation-builder.spec.ts
@@ -1,5 +1,10 @@
 import { faker } from '@faker-js/faker'
-import { EmailValidation, MinLengthValidation, RequiredFieldValidation } from 'validation/validators'
+import {
+  EmailValidation,
+  MinLengthValidation,
+  RequiredFieldValidation
+} from 'validation/validators'
+import { CompareFieldsValidation } from '../compare-fields/compare-fields'
 import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
@@ -19,13 +24,33 @@ describe('ValidationBuilder', () => {
     const field = faker.random.word()
     const minLength = Number(faker.random.numeric())
     const validations = sut.field(field).minLength(minLength).build()
-    expect(validations).toStrictEqual([new MinLengthValidation(field, minLength)])
+    expect(validations).toStrictEqual([
+      new MinLengthValidation(field, minLength)
+    ])
+  })
+
+  it('should return CompareFieldsValidation', () => {
+    const field = faker.random.alphaNumeric(4)
+    const fieldToCompare = faker.random.alphaNumeric(7)
+    const validations = sut
+      .field(field)
+      .compareWithField(fieldToCompare)
+      .build()
+
+    expect(validations).toStrictEqual([
+      new CompareFieldsValidation(field, fieldToCompare)
+    ])
   })
 
   it('should return a list of field validators', () => {
     const field = faker.random.word()
     const minLength = Number(faker.random.numeric())
-    const validations = sut.field(field).required().email().minLength(minLength).build()
+    const validations = sut
+      .field(field)
+      .required()
+      .email()
+      .minLength(minLength)
+      .build()
 
     expect(validations).toStrictEqual([
       new RequiredFieldValidation(field),
