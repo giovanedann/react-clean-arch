4fc6d978a71e1c385375f1a6231687c357f31452
Giovane <giovanedaniell@gmail.com>
2023-03-02 15:15:24 -0300

    test: add sign up validation factory

---
+++ src/main/factories/pages/SignUp/sign-up-validation-factory.spec.ts
@@ -0,0 +1,20 @@
+import { ValidationBuilder, ValidationComposite } from 'validation/validators'
+import signUpValidationFactory from './sign-up-validation-factory'
+
+describe('SignUpValidationFactory', () => {
+  it('should create ValidationComposite with the right validations', () => {
+    const composite = signUpValidationFactory()
+    expect(composite).toStrictEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('name').required().build(),
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().minLength(5).build(),
+        ...ValidationBuilder.field('passwordConfirmation')
+          .required()
+          .compareWithField('password')
+          .minLength(5)
+          .build()
+      ])
+    )
+  })
+})
