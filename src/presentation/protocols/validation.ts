eb3d2fcf6bd25265760a42e30939e5d9e583a9be
Giovane <giovanedaniell@gmail.com>
2023-03-02 15:09:59 -0300

    style: change validate method parameter to a more descriptive name

--- src/presentation/protocols/validation.ts
+++ src/presentation/protocols/validation.ts
@@ -1,3 +1,3 @@
-export interface Validation {
-  validate: (fieldName: string, fieldValue: string) => string
+export interface Validation<T> {
+  validate: (fieldName: string, object: T) => string
 }
