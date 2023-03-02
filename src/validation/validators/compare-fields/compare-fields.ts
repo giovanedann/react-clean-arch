abca52672485e55a84a680e3b44d24c68b0d8edf
Giovane <giovanedaniell@gmail.com>
2023-03-02 14:58:54 -0300

    refactor: add generic to CompareFieldValidation and change strategy

--- src/validation/validators/compare-fields/compare-fields.ts
+++ src/validation/validators/compare-fields/compare-fields.ts
@@ -1,10 +1,12 @@
 import { InvalidFieldError } from 'validation/errors'
 import { type FieldValidation } from 'validation/protocols/field-validation'
 
-export class CompareFieldsValidation implements FieldValidation {
-  constructor(readonly field: string, private readonly compareWith: string) {}
+export class CompareFieldsValidation<T> implements FieldValidation<T> {
+  constructor(readonly field: keyof T, private readonly compareWith: keyof T) {}
 
-  validate(value: string): Error | null {
-    return value === this.compareWith ? null : new InvalidFieldError(this.field)
+  validate(object: T): Error | null {
+    return object[this.field] === object[this.compareWith]
+      ? null
+      : new InvalidFieldError(this.field as string)
   }
 }
