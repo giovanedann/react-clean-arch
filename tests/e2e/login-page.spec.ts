87e2c0923fd6f4db48009dedcfa0a0f0c97553ac
Giovane <giovanedaniell@gmail.com>
2023-03-03 13:45:03 -0300

    test(e2e): ensure accessToken is saved on localStorage after Login submit succeeds

--- tests/e2e/login-page.spec.ts
+++ tests/e2e/login-page.spec.ts
@@ -141,4 +141,31 @@ test.describe('Login Page', () => {
 
     await expect(page).toHaveURL('http://localhost:3000/sign-up')
   })
+
+  test('should save the access token on localStorage after submit succeeds', async ({
+    page
+  }) => {
+    const accessToken = faker.datatype.uuid()
+
+    await page.route('http://localhost:5050/api/login', async (route) => {
+      const json = {
+        accessToken
+      }
+      await route.fulfill({ status: 200, json })
+    })
+
+    await page
+      .getByPlaceholder(/enter your e-mail/i)
+      .fill(faker.internet.email())
+
+    await page
+      .getByPlaceholder(/enter your password/i)
+      .fill(faker.internet.password(6))
+
+    await page.getByRole('button', { name: /sign in/i }).click()
+
+    expect(await page.evaluate(() => window.localStorage)).toStrictEqual({
+      accessToken
+    })
+  })
 })
