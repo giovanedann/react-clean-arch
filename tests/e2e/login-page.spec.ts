708376df8bdc35e7e1c4fa68942b1ea2a73203bf
Giovane <giovanedaniell@gmail.com>
2023-03-03 12:04:27 -0300

    test(e2e): ensure Login displays invalid credentials on 401

--- tests/e2e/login-page.spec.ts
+++ tests/e2e/login-page.spec.ts
@@ -76,4 +76,21 @@ test.describe('Login Page', () => {
     await page.getByRole('button', { name: /sign in/i }).click()
     await expect(page.getByText(/loading\.../i)).toBeInViewport()
   })
+
+  test('should display invalid credentials error on 401', async ({ page }) => {
+    await page.route('http://localhost:5050/api/login', async (route) => {
+      await route.fulfill({ status: 401 })
+    })
+
+    await page
+      .getByPlaceholder(/enter your e-mail/i)
+      .fill(faker.internet.email())
+
+    await page
+      .getByPlaceholder(/enter your password/i)
+      .fill(faker.internet.password(6))
+
+    await page.getByRole('button', { name: /sign in/i }).click()
+    await expect(page.getByText(/invalid credentials/i)).toBeVisible()
+  })
 })
