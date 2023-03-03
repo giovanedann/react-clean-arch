68dd949fc8988c40e9b30ce94927a777a25daac4
Giovane <giovanedaniell@gmail.com>
2023-03-03 12:04:00 -0300

    test(e2e): ensure Login loading status is displayed during submit

--- tests/e2e/login-page.spec.ts
+++ tests/e2e/login-page.spec.ts
@@ -1,6 +1,15 @@
 import { faker } from '@faker-js/faker'
 import { test, expect } from '@playwright/test'
 
+async function delay(ms: number): Promise<void> {
+  const waiter = new Promise((resolve) => {
+    setTimeout(function () {
+      resolve('true')
+    }, ms)
+  })
+  await waiter
+}
+
 test.describe('Login Page', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000/login')
@@ -26,9 +35,10 @@ test.describe('Login Page', () => {
 
     await page
       .getByPlaceholder(/enter your password/i)
-      .fill(faker.internet.password())
+      .fill(faker.internet.password(6))
 
     await expect(page.getByText(/🟢/i)).toHaveCount(2)
+    await expect(page.getByRole('button', { name: /sign in/i })).toBeEnabled()
   })
 
   test('should disable the submit button on invalid form values', async ({
@@ -43,5 +53,27 @@ test.describe('Login Page', () => {
       .fill(faker.random.alphaNumeric(4))
 
     await expect(page.getByRole('button', { name: /sign in/i })).toBeDisabled()
+  })
+
+  test('should display the loader after submit', async ({ page }) => {
+    await page.route('http://localhost:5050/api/login', async (route) => {
+      const json = {
+        accessToken: faker.datatype.uuid()
+      }
+      route.request()
+      await delay(1000)
+      await route.fulfill({ json })
+    })
+
+    await page
+      .getByPlaceholder(/enter your e-mail/i)
+      .fill(faker.internet.email())
+
+    await page
+      .getByPlaceholder(/enter your password/i)
+      .fill(faker.internet.password(6))
+
+    await page.getByRole('button', { name: /sign in/i }).click()
+    await expect(page.getByText(/loading\.../i)).toBeInViewport()
   })
 })
