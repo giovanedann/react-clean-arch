e910027458feb44c1ed9107f2eb1425ab39b4b1e
Giovane <giovanedaniell@gmail.com>
2023-03-05 18:56:21 -0300

    feat: ensure HttpClient get method return the correct values

--- src/infra/http/client/http-client.ts
+++ src/infra/http/client/http-client.ts
@@ -1,4 +1,5 @@
 import {
+  type HttpGetClient,
   type HttpGetParams,
   type HttpPostClient,
   type HttpPostParams,
@@ -6,7 +7,7 @@ import {
 } from 'data/protocols/http'
 import axios, { type AxiosResponse } from 'axios'
 
-export class HttpClient implements HttpPostClient {
+export class HttpClient implements HttpPostClient, HttpGetClient<any> {
   async post(params: HttpPostParams): Promise<HttpResponse> {
     let axiosResponse: AxiosResponse
 
@@ -22,7 +23,12 @@ export class HttpClient implements HttpPostClient {
     }
   }
 
-  async get({ url }: HttpGetParams): Promise<void> {
-    await axios.get(url)
+  async get({ url }: HttpGetParams): Promise<HttpResponse> {
+    const axiosResponse = await axios.get(url)
+
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    }
   }
 }
