3fe6c87615ae1f6355df7cd7764888118e07cdf6
Giovane <giovanedaniell@gmail.com>
2023-03-05 18:05:35 -0300

    test: ensure RemoteLoadSurveyList returns a list of survey models

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -2,7 +2,10 @@ import { faker } from '@faker-js/faker'
 import { HttpStatusCode } from 'data/protocols/http'
 import { UnexpectedError } from 'domain/errors'
 import { type SurveyModel } from 'domain/models'
-import { HttpGetClientSpy } from 'tests/mocks/data/protocols/http/http-get-client'
+import {
+  HttpGetClientSpy,
+  mockLoadSurveyList
+} from 'tests/mocks/data/protocols/http/http-get-client'
 import { RemoteLoadSurveyList } from './remote-load-survey-list'
 
 type SutTypes = {
@@ -13,6 +16,8 @@ type SutTypes = {
 function createSut(url: string = faker.internet.url()): SutTypes {
   const httpGetClientSpy = new HttpGetClientSpy<SurveyModel[]>()
   const sut = new RemoteLoadSurveyList(url, httpGetClientSpy)
+
+  httpGetClientSpy.response.body = []
 
   return { sut, httpGetClientSpy }
 }
@@ -55,5 +60,17 @@ describe('RemoteLoadSurveyList', () => {
     }
 
     await expect(sut.loadAll()).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should return a list of SurveyModels if HttpGetClient returns 200', async () => {
+    const { httpGetClientSpy, sut } = createSut()
+
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: mockLoadSurveyList()
+    }
+
+    const surveyList = await sut.loadAll()
+    expect(surveyList).toStrictEqual(httpGetClientSpy.response.body)
   })
 })
