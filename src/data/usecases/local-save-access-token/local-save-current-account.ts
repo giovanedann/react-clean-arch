1f488001c2b59d06cac3b1fdd806c657158cf979
Giovane <giovanedaniell@gmail.com>
2023-03-06 20:14:11 -0300

    refactor(usecases/factories): change all affected files to new AccountModel interface

---
+++ src/data/usecases/local-save-access-token/local-save-current-account.ts
@@ -0,0 +1,11 @@
+import { type SetStorage } from 'data/protocols/cache'
+import { type AccountModel } from 'domain/models'
+import { type SaveCurrentAccount } from 'domain/usecases'
+
+export class LocalSaveCurrentAccount implements SaveCurrentAccount {
+  constructor(private readonly setStorage: SetStorage) {}
+
+  async save(account: AccountModel): Promise<void> {
+    await this.setStorage.set('currentAccount', JSON.stringify(account))
+  }
+}
--- src/main/factories/usecases/save-access-token/save-access-token-factory.ts
+++
@@ -1,7 +0,0 @@
-import { LocalSaveAccessToken } from 'data/usecases/local-save-access-token/local-save-access-token'
-import { type SaveAccessToken } from 'domain/usecases'
-import localStorageAdapterFactory from 'main/factories/cache/local-storage-adapter-factory'
-
-export default function localSaveAccessTokenFactory(): SaveAccessToken {
-  return new LocalSaveAccessToken(localStorageAdapterFactory())
-}
