8d3ee38aac5862cfc6cc8dd5383cc6b25eaf6ba9
Giovane <giovanedaniell@gmail.com>
2023-03-14 19:46:02 -0300

    refactor(decorators): move AuthHttpGetClient decorator sut creation to a helper function

--- src/main/decorators/auth-http-get-client/auth--http-get-client-decorator.spec.ts
+++ src/main/decorators/auth-http-get-client/auth--http-get-client-decorator.spec.ts
@@ -2,10 +2,21 @@ import { GetStorageSpy } from 'tests/mocks/data/protocols/cache/get-storage'
 import { mockGetRequest } from 'tests/mocks/data/protocols/http/http-get-client'
 import { AuthorizeHttpGetClientDecorator } from './auth--http-get-client-decorator'
 
+type SutTypes = {
+  getStorageSpy: GetStorageSpy
+  sut: AuthorizeHttpGetClientDecorator
+}
+
+function createSut(): SutTypes {
+  const getStorageSpy = new GetStorageSpy()
+  const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+
+  return { getStorageSpy, sut }
+}
+
 describe('AuthorizeHttpGetClientDecorator', () => {
   it('should call GetStorage with the right key', async () => {
-    const getStorageSpy = new GetStorageSpy()
-    const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+    const { sut, getStorageSpy } = createSut()
 
     await sut.get(mockGetRequest())
 
