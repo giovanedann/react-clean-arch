b9c3c5cfd4a2ac90505ea37da339400511fbddd1
Giovane <giovanedaniell@gmail.com>
2023-03-16 20:54:40 -0300

    refactor(hooks): refactor useHandleForbidden to use useLogout

--- src/presentation/hooks/useHandleForbidden.ts
+++ src/presentation/hooks/useHandleForbidden.ts
@@ -1,18 +1,15 @@
 import { AccessDeniedError } from 'domain/errors'
-import { useApi } from 'presentation/contexts/api'
-import { useNavigate } from 'react-router-dom'
+import useLogout from './useLogout'
 
 type Callback = (error: Error) => void
 type Result = (error: Error) => void
 
 export default function useHandleForbidden(callback: Callback): Result {
-  const navigate = useNavigate()
-  const { saveCurrentAccount } = useApi()
+  const logout = useLogout()
 
   return (error: Error) => {
     if (error instanceof AccessDeniedError) {
-      saveCurrentAccount(null)
-      navigate('/login')
+      logout()
     } else {
       callback(error)
     }
