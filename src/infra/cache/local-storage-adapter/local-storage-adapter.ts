2ffc03fdc1521614977c58750d76815ff7ff68ec
Giovane <giovanedaniell@gmail.com>
2023-03-15 23:19:19 -0300

    feat: ensure LocalStorageAdapter remove the key from localStorage if value parameter of set method is null

--- src/infra/cache/local-storage-adapter/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter/local-storage-adapter.ts
@@ -2,6 +2,11 @@ import { type SetStorage, type GetStorage } from 'data/protocols/cache'
 
 export class LocalStorageAdapter implements SetStorage, GetStorage {
   set(key: string, value: any): void {
+    if (!value) {
+      localStorage.removeItem(key)
+      return
+    }
+
     localStorage.setItem(key, JSON.stringify(value))
   }
 
