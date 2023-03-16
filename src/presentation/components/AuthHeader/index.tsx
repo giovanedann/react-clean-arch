b662c0d842328982a2f3cf1a623f58b0561e5223
Giovane <giovanedaniell@gmail.com>
2023-03-15 23:53:01 -0300

    feat(components): ensure AuthHeader redirects user to login and clear currentAccount from localStorage on logout click

--- src/presentation/components/AuthHeader/index.tsx
+++ src/presentation/components/AuthHeader/index.tsx
@@ -1,15 +1,29 @@
+import { memo, type MouseEvent } from 'react'
 import Logo from 'presentation/components/Logo'
-import { memo } from 'react'
 import styles from './styles.module.scss'
+import { useApi } from 'presentation/contexts/api'
+import { useNavigate } from 'react-router-dom'
 
 function AuthHeader(): JSX.Element {
+  const { saveCurrentAccount } = useApi()
+  const navigate = useNavigate()
+
+  function handleLogout(event: MouseEvent<HTMLAnchorElement>): void {
+    event.preventDefault()
+
+    saveCurrentAccount(null)
+    navigate('/login')
+  }
+
   return (
     <header className={styles.headerWrapper}>
       <div className={styles.headerContent}>
         <Logo />
         <div className={styles.logoutWrapper}>
           <span>User</span>
-          <a href="#">Logout</a>
+          <a href="#" onClick={handleLogout}>
+            Logout
+          </a>
         </div>
       </div>
     </header>
