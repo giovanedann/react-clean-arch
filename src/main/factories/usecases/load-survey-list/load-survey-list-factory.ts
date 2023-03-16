805639f79e67f60afffd5c141bc0b4b36841031b
Giovane <giovanedaniell@gmail.com>
2023-03-15 22:29:22 -0300

    feat: add AuthHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/load-survey-list-factory.ts
@@ -1,10 +1,10 @@
 import { RemoteLoadSurveyList } from 'data/usecases/load-survey-list/remote-load-survey-list'
+import { authHttpGetClientDecoratorFactory } from 'main/factories/decorators/auth-http-get-client-decorator-factory'
 import apiUrlFactory from 'main/factories/http/api-url-factory'
-import httpClientFactory from 'main/factories/http/http-client-factory'
 
 export default function remoteLoadSurveyListFactory(): RemoteLoadSurveyList {
   return new RemoteLoadSurveyList(
     apiUrlFactory('/surveys'),
-    httpClientFactory()
+    authHttpGetClientDecoratorFactory()
   )
 }
