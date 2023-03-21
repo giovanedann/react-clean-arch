6e60cc49b93b793b675a632776a8aac88f409b25
Giovane <giovanedaniell@gmail.com>
2023-03-21 00:38:46 -0300

    test(e2e): ensure SurveyList load the survey item skeleton while loading surveys

--- tests/e2e/survey-list.spec.ts
+++ tests/e2e/survey-list.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { mockAccountModel } from 'tests/mocks/domain/models/account'
+import delay from '../utils/delay'
 
 const URL = 'http://localhost:3000/'
 const API_URL = 'http://localhost:5050/api/surveys'
@@ -33,5 +34,27 @@ test.describe('SurveyList page', () => {
     await page.goto(URL)
 
     await expect(page.getByText(account.name)).toBeVisible()
+  })
+
+  test('should load the survey items skeleton while loading surveys', async ({
+    page
+  }) => {
+    const account = mockAccountModel()
+
+    await page.addInitScript((value) => {
+      window.localStorage.setItem('currentAccount', value)
+    }, JSON.stringify(account))
+
+    await page.route(API_URL, async (route) => {
+      route.request()
+      await delay(1000)
+      await route.fulfill({
+        status: 200
+      })
+    })
+
+    await page.goto(URL)
+
+    await expect(page.getByTitle(/survey list skeleton/i)).toBeVisible()
   })
 })
