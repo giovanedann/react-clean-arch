e223a50e121228906c7dbb26ae15ea0ae92ce21b
Giovane <giovanedaniell@gmail.com>
2023-03-21 00:37:21 -0300

    test(e2e): ensure SurveyList presents the right user name on header

--- tests/e2e/survey-list.spec.ts
+++ tests/e2e/survey-list.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test'
+import { mockAccountModel } from 'tests/mocks/domain/models/account'
 
 const URL = 'http://localhost:3000/'
 const API_URL = 'http://localhost:5050/api/surveys'
@@ -14,5 +15,23 @@ test.describe('SurveyList page', () => {
     await page.goto(URL)
 
     await expect(page).toHaveURL('http://localhost:3000/login')
+  })
+
+  test('should present the right name on header', async ({ page }) => {
+    const account = mockAccountModel()
+
+    await page.addInitScript((value) => {
+      window.localStorage.setItem('currentAccount', value)
+    }, JSON.stringify(account))
+
+    await page.route(API_URL, async (route) => {
+      await route.fulfill({
+        status: 200
+      })
+    })
+
+    await page.goto(URL)
+
+    await expect(page.getByText(account.name)).toBeVisible()
   })
 })
