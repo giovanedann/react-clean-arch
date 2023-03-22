8144e7db4cd22859791fe743970c3962f156387d
Giovane <giovanedaniell@gmail.com>
2023-03-22 00:50:58 -0300

    feat(pages): add SurveyResult page initial template

--- src/main/Routes/index.tsx
+++ src/main/Routes/index.tsx
@@ -5,6 +5,7 @@ import signUpPageFactory from 'main/factories/pages/SignUp/sign-up-factory'
 import surveyListPageFactory from 'main/factories/pages/SurveyList/survey-list-factory'
 import { ApiProvider } from 'presentation/contexts/api'
 import { FormProvider } from 'presentation/contexts/form'
+import SurveyResult from 'presentation/pages/SurveyResult'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'styles/global.scss'
 import PrivateRoute from './PrivateRoute'
@@ -17,6 +18,7 @@ export default function Router(): JSX.Element {
           <Routes>
             <Route path="/" element={<PrivateRoute />}>
               <Route path="/" element={surveyListPageFactory()} />
+              <Route path="/surveys" element={<SurveyResult />} />
             </Route>
             <Route path="/login" element={loginPageFactory()} />
             <Route path="/sign-up" element={signUpPageFactory()} />
---
+++ src/presentation/pages/SurveyResult/index.tsx
@@ -0,0 +1,39 @@
+import AuthHeader from 'presentation/components/AuthHeader'
+import FlipMove from 'react-flip-move'
+import styles from './styles.module.scss'
+
+export default function SurveyResult(): JSX.Element {
+  return (
+    <main className={styles.wrapper}>
+      <AuthHeader />
+      <div className={styles.contentContainer}>
+        <h2>What is your favorite web framework?</h2>
+        <FlipMove className={styles.answersList}>
+          <li>
+            <img
+              src="https://logospng.org/download/react/logo-react-1024.png"
+              alt=""
+            />
+            <span className={styles.answer}>ReactJS</span>
+            <span className={styles.percentage}>40%</span>
+          </li>
+          <li className={styles.voted}>
+            <img src="https://cdn.cdnlogo.com/logos/n/80/next-js.svg" alt="" />
+            <span className={styles.answer}>NextJS</span>
+            <span className={styles.percentage}>50%</span>
+          </li>
+          <li>
+            <img
+              src="https://logospng.org/wp-content/uploads/vue-js.png"
+              alt=""
+            />
+            <span className={styles.answer}>VueJS</span>
+            <span className={styles.percentage}>10%</span>
+          </li>
+        </FlipMove>
+
+        <button type="button">Voltar</button>
+      </div>
+    </main>
+  )
+}
