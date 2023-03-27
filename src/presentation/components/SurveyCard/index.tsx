e905455d5223951a837cfc3f96d597fcefb454f0
Giovane <giovanedaniell@gmail.com>
2023-03-27 20:47:26 -0300

    refactor: add DateCard to SurveyCard

--- src/presentation/components/SurveyCard/index.tsx
+++ src/presentation/components/SurveyCard/index.tsx
@@ -1,6 +1,7 @@
 import { type LoadSurveyList } from 'domain/usecases'
 import { memo, useMemo } from 'react'
 import { IoMdThumbsUp } from 'react-icons/io'
+import DateCard from '../DateCard'
 import styles from './styles.module.scss'
 
 type Props = Pick<LoadSurveyList.Model, 'date' | 'didAnswer' | 'question'>
@@ -21,13 +22,7 @@ function SurveyCard({ didAnswer, question, date }: Props): JSX.Element {
           size={25}
           title="Status icon"
         />
-        <time>
-          <span className={styles.day}>{date.getDay()}</span>
-          <span className={styles.month}>
-            {date.toLocaleString('en-US', { month: 'short' })}
-          </span>
-          <span className={styles.year}>{date.getFullYear()}</span>
-        </time>
+        <DateCard date={date} />
         <p>{question}</p>
       </div>
       <footer>See results</footer>
