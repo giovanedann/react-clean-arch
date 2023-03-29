09c8455f0d822b92500332db52ae95e7562a5c4c
Giovane <giovanedaniell@gmail.com>
2023-03-28 23:58:13 -0300

    test(e2e): ensure SurveyResult shows the skeleton while loading

---
+++ tests/e2e/survey-result-page.spec.ts
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker'
+import { test, expect } from '@playwright/test'
+import { mockAccountModel } from 'tests/mocks/domain/models/account'
+import delay from '../utils/delay'
+
+const SURVEY_RESULT_API_URL = (id: string): string =>
+  `http://localhost:5050/api/surveys/${id}/results`
+const SURVEY_RESULT_CLIENT_URL = (id: string): string =>
+  `http://localhost:3000/surveys/${id}`
+
+test.describe('SurveyResult page', () => {
+  // Test cases for LoadSurveyResult use case
+  test.describe('LoadSurveyResult', () => {
+    test.beforeEach(async ({ page }) => {
+      const account = mockAccountModel()
+
+      await page.addInitScript((value) => {
+        window.localStorage.setItem('currentAccount', value)
+      }, JSON.stringify(account))
+    })
+
+    test('should show the skeleton while loading', async ({ page }) => {
+      const surveyId = faker.datatype.uuid()
+
+      await page.route(SURVEY_RESULT_API_URL(surveyId), async (route) => {
+        route.request()
+        await delay(1000)
+        await route.fulfill({
+          status: 200
+        })
+      })
+
+      await page.goto(SURVEY_RESULT_CLIENT_URL(surveyId))
+
+      await expect(page.getByTitle(/survey result skeleton/i)).toBeVisible()
+    })
+  })
+})
