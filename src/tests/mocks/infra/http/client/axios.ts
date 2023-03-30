db7334891755b5a200e15c760ba1ff5a21a7dc54
Giovane <giovanedaniell@gmail.com>
2023-03-29 21:40:20 -0300

    refactor(mocks): adapt mocks to new infra/usecase structure

--- src/tests/mocks/infra/http/client/axios.ts
+++ src/tests/mocks/infra/http/client/axios.ts
@@ -14,7 +14,6 @@ export const mockHttpResponse = (): HttpResponseMock => ({
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>
 
-  mockedAxios.post.mockResolvedValue(mockHttpResponse())
-  mockedAxios.get.mockResolvedValue(mockHttpResponse())
+  mockedAxios.request.mockResolvedValue(mockHttpResponse())
   return mockedAxios
 }
