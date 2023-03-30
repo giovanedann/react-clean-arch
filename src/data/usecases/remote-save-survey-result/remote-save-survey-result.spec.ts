b9755e2a619cfbb9b98eb95b559d1f76f996fb52
Giovane <giovanedaniell@gmail.com>
2023-03-29 22:44:46 -0300

    test: ensure RemoteSaveSurveyResult calls httpClient.request with correct url and method

---
+++ src/data/usecases/remote-save-survey-result/remote-save-survey-result.spec.ts
@@ -0,0 +1,42 @@
+import { faker } from '@faker-js/faker'
+import { type SaveSurveyResult } from 'domain/usecases/save-survey-result'
+import { HttpClientSpy } from 'tests/mocks/data/protocols/http/http-client'
+import { mockSurveyResultModel } from 'tests/mocks/domain/models/load-survey-result'
+import { RemoteSaveSurveyResult } from './remote-save-survey-result'
+
+type SutTypes = {
+  sut: RemoteSaveSurveyResult
+  httpClientSpy: HttpClientSpy<RemoteSaveSurveyResult.Model>
+  sutParams: SaveSurveyResult.Params
+}
+
+type SutParams = {
+  url?: string
+}
+
+function createSut({ url = faker.internet.url() }: SutParams): SutTypes {
+  const httpClientSpy = new HttpClientSpy<RemoteSaveSurveyResult.Model>()
+
+  const sutParams = { answer: faker.random.word() }
+
+  httpClientSpy.response = {
+    ...httpClientSpy.response,
+    body: mockSurveyResultModel()
+  }
+
+  const sut = new RemoteSaveSurveyResult(url, httpClientSpy)
+
+  return { sut, httpClientSpy, sutParams }
+}
+
+describe('RemoteSaveSurveyResult', () => {
+  it('should call HttpClient with correct URL and method', () => {
+    const url = faker.internet.url()
+    const { httpClientSpy, sut, sutParams } = createSut({ url })
+
+    sut.save(sutParams)
+
+    expect(httpClientSpy.url).toEqual(url)
+    expect(httpClientSpy.method).toEqual('put')
+  })
+})
